ff3b6becb45ca0ec73a43d7ebfa67e06fef0dab5
Jackson de Campos <jacksondecampos@gmail.com>
2021-12-06 11:59:39 -0500

    counter/multiple predators

--- pursuit_mdp.py
+++ pursuit_mdp.py
@@ -7,6 +7,7 @@ from typing import Tuple, List, Set, Callable, Iterable
 import itertools
 import random
 import copy
+from collections import Counter
 
 # Other imports.
 import matplotlib.pyplot as plt
@@ -32,15 +33,16 @@ class PursuitAction:
 
 class PursuitState(State):
     def __init__(self, predator_locs: Iterable[Tuple[int, int]], prey_loc: Tuple[int, int]):
-        self.predator_locs = set(predator_locs)
+        self.predator_locs = Counter(predator_locs)
         self.prey_loc = prey_loc
         State.__init__(self, data=[self.predator_locs, self.prey_loc])
 
     def __hash__(self):
-        return hash((tuple(self.predator_locs), self.prey_loc))
+        return hash((tuple(sorted(self.predator_locs.items())), self.prey_loc))
 
     def is_terminal(self):
-        return self.prey_loc in self.predator_locs
+        # both predators on top of prey
+        return self.prey_loc in self.predator_locs and len(self.predator_locs) == 1
 
     def __str__(self):
         return f"<pred locs: {self.predator_locs}, prey_loc: {self.prey_loc}>"
@@ -66,38 +68,34 @@ class PursuitMDP(MDP):
         :param x_limit: range of x values, inclusive -- defaults to (0, width)
         :param y_limit: range of y values, inclusive -- defaults to (0, height)
         """
-        if rand_init:
-            init_predator_locs = [(randrange(width), randrange(height)) for _ in range(num_predators)]
-            init_prey_loc = (randrange(width), randrange(height))
-            while init_prey_loc in init_predator_locs:
-                init_prey_loc = (randrange(width), randrange(height))
+        self.width = width
+        self.height = height
+        self.x_limit = max(0, x_limit[0]), min(width - 1, x_limit[1])
+        self.y_limit = max(0, y_limit[0]), min(height - 1, y_limit[1])
+        self.num_predators = num_predators
+        self.wall_locs = wall_locs
+        self.default_reward = default_reward
 
-        self.init_predator_locs = set(init_predator_locs)
-        self.init_state = PursuitState(self.init_predator_locs, init_prey_loc)
+        if rand_init:
+            self.init_state = random.choice(self.states)
+        else:
+            self.init_state = PursuitState(init_predator_locs, init_prey_loc)
+
         self.actions = [PursuitAction(a) for a in list(itertools.product(self.PRIMITIVE_ACTIONS, repeat=num_predators))]
         MDP.__init__(self, self.actions, self._transition_func, self._reward_func,
                      init_state=self.init_state, gamma=gamma)
 
-        self.width = width
-        self.height = height
-        self.num_predators = num_predators
-
-        self.wall_locs = wall_locs
-        self.init_prey_loc = init_prey_loc
-        self.default_reward = default_reward
-        self.x_limit = max(0, x_limit[0]), min(width - 1, x_limit[1])
-        self.y_limit = max(0, y_limit[0]), min(height - 1, y_limit[1])
 
     @property
     def states(self):
         x_lo, x_hi = self.x_limit[0], self.x_limit[1] + 1
         y_lo, y_hi = self.y_limit[0], self.y_limit[1] + 1
         locs = [(i, j) for i in range(x_lo, x_hi) for j in range(y_lo, y_hi)]
-        pred_loc_sets = [set(pred_loc_list) for pred_loc_list in itertools.product(locs, repeat=self.num_predators)
-                        if len(set(pred_loc_list)) == self.num_predators]  # no collisions
-        states = [PursuitState(pred_loc_set, prey_loc)
-                  for pred_loc_set in pred_loc_sets for prey_loc in locs
-                  if prey_loc not in pred_loc_set]
+
+        pred_loc_lists = itertools.product(locs, repeat=self.num_predators)
+
+        states = [PursuitState(pred_loc_list, prey_loc)
+                  for pred_loc_list in pred_loc_lists for prey_loc in locs]
         return states
 
     def _transition_func(self, state: PursuitState, action: PursuitAction) -> PursuitState:
@@ -106,19 +104,15 @@ class PursuitMDP(MDP):
 
         new_prey_loc = self._apply_action(state.prey_loc, self.prey_policy(state))
 
-        pred_locs_list = sorted(list(state.predator_locs))
-        if not len(pred_locs_list) == self.num_predators:
+        pred_locs_counter = state.predator_locs
+        if not sum(pred_locs_counter.values()) == self.num_predators:
             raise RuntimeError("Predators list length does not match number of predators")
         if not action.length == self.num_predators:
             raise RuntimeError("Action length does not match number of predators")
-        new_pred_locs = set(
-            self._apply_action(loc, primitive_action)
-            for loc, primitive_action in zip(state.predator_locs, action.as_list()))
-        if len(new_pred_locs) < self.num_predators:
-            # collision, revert to old positions (no one moves)
-            new_pred_locs = state.predator_locs
+        old_predator_locs_list = sorted([k for k, v in state.predator_locs.items() for _ in range(v)])
+        new_pred_locs_list = [self._apply_action(l, a) for (l, a) in zip(old_predator_locs_list, action.as_list())]
 
-        return PursuitState(predator_locs=new_pred_locs, prey_loc=new_prey_loc)
+        return PursuitState(predator_locs=new_pred_locs_list, prey_loc=new_prey_loc)
 
     def _apply_action(self, loc: Tuple[int, int], action: PrimitiveAction) -> Tuple[int, int]:
         """Move in specified direction, as long as wall is not there, and clamp"""
